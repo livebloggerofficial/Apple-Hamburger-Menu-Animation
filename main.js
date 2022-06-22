921b345fbaf665ee6abd8f729178af06b15d670e
Live Blogger <61611286+livebloggerofficial@users.noreply.github.com>
2022-06-23 02:11:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const menuContainer = document.querySelector(".menu-container");
+const menuItemsContainer = document.querySelector(".menu-items-container");
 
 menuContainer.addEventListener("click", () => {
   menuContainer.classList.toggle("active");
+
+  if (menuContainer.classList.contains("active")) {
+    menuItemsContainer.classList.add("active");
+  } else {
+    menuItemsContainer.classList.remove("active");
+  }
 });
